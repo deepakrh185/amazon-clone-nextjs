b12bb1e19ffbde162c0b798cce14eddb3c9437e2
Deepak <deepsrh1801@gmail.com>
2021-05-21 16:28:54 +0530

    done with frontend part

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,17 +3,80 @@ import Product from "./Product";
 function ProductFeed({ products }) {
   return (
     <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-      {products.map(({ id, title, category, image, price, description }) => (
-        <Product
-          key={id}
-          id={id}
-          title={title}
-          category={category}
-          description={description}
-          price={price}
-          image={image}
-        />
-      ))}
+      {products
+        .slice(0, 4)
+        .map(({ id, title, category, image, price, description }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            category={category}
+            description={description}
+            price={price}
+            image={image}
+          />
+        ))}
+      <img src="https://links.papareact.com/dyz" className="md:col-span-full" />
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, category, image, price, description }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              category={category}
+              description={description}
+              price={price}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5, 9)
+        .map(({ id, title, category, image, price, description }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            category={category}
+            description={description}
+            price={price}
+            image={image}
+          />
+        ))}
+      <img
+        src="https://images-eu.ssl-images-amazon.com/images/G/31/img18/Fresh/Dec20/CatStore/SF_Header_1500x230_4.jpg"
+        className="md:col-span-full"
+      />
+      <div className="md:col-span-2">
+        {products
+          .slice(9, 10)
+          .map(({ id, title, category, image, price, description }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              category={category}
+              description={description}
+              price={price}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(10, products.length)
+        .map(({ id, title, category, image, price, description }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            category={category}
+            description={description}
+            price={price}
+            image={image}
+          />
+        ))}
     </div>
   );
 }
