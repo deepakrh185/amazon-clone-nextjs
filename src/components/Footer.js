3dbdad5cd6a9d1d7c92864f00ddabcedba2acac7
Deepak <deepsrh1801@gmail.com>
2021-05-27 11:55:13 +0530

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <div className="md:flex-row flex-col max-w-screen-2xl mx-auto py-5 text-center md:justify-center px-5">
+      <div className="text-gray-800 md:mb-4 w-auto">
+        Developed with ♥ by{" "}
+        <a className="underline" href="https://lucid-hoover-b8720c.netlify.app/">
+          Deepak RH
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import ProductFeed from "../components/ProductFeed";
 import { getSession } from "next-auth/client";
+import Footer from "../components/Footer";
 
 export default function Home({ products }) {
   return (
@@ -15,6 +16,7 @@ export default function Home({ products }) {
         <Banner />
         <ProductFeed products={products} />
       </main>
+      <Footer />
     </div>
   );
 }
@@ -24,6 +26,7 @@ export async function getServerSideProps(context) {
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
   );
+
   return {
     props: {
       products,
