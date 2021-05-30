76be9326ce4f59e5a27c708389cc63d3c46ab0ac
Deepak <deepsrh1801@gmail.com>
2021-05-30 17:22:26 +0530

    code change

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,70 +1,89 @@
+//this will be fired on successfull stripe payment
+//this is backend and not frontend firebase
+//YOU CAN GET FULL DOCUMENTATION ON STRIPE WEBHOOK
+
 import { buffer } from "micro";
 import * as admin from "firebase-admin";
 
-// Secure a connection to firebase
+//Secure a connection to firebase from the backend
 const serviceAccount = require("../../../permission.json");
 
+//as it is next js check if the app has already been initialized or not
+//to save for doing it again
 const app = !admin.apps.length
   ? admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     })
   : admin.app();
 
-// Stripe
+//Est a conn to stripe
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const endpointSecurit = process.env.STRIPE_SIGNING_SECRET;
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
 const fulfillOrder = async (session) => {
-  console.log("Fullfilling Order!!!", session);
+  //console.log('Fulfilling order', session)
+
+  //new addition from github
+  const images = JSON.parse(session.metadata.images).map((image) =>
+    JSON.stringify(image)
+  );
 
   return app
     .firestore()
-    .collection("users")
+    .collection("next-amazon-users")
     .doc(session.metadata.email)
     .collection("orders")
     .doc(session.id)
     .set({
       amount: session.amount_total / 100,
-      amount_shipping: session.total_details_amount_shipping / 100,
-      images: JSON.parse(session.metadata.images),
-      title: JSON.parse(session.metadata.titles),
+      amount_shipping: session.total_details.amount_shipping / 100,
+      images: images,
+      //images: JSON.parse(session.metadata.images),
       timestamp: admin.firestore.FieldValue.serverTimestamp(),
     })
     .then(() => {
-      console.log(`SUCCESS: Order ${session.id} has been added to DB!`);
-    });
+      console.log(`SUCCESS: Order ${session.id} has been added to firestore`);
+    })
+    .catch((err) => console.log("Error in insertion db !", err.message));
 };
 
 export default async (req, res) => {
   if (req.method === "POST") {
+    //this buffer is to store certififcate that the webhook will emit
+    //this cert tells us this is a verified stripe success event and not a fraud event
     const requestBuffer = await buffer(req);
     const payload = requestBuffer.toString();
     const sig = req.headers["stripe-signature"];
 
     let event;
 
-    // Verify (came from stripe)
+    //verify that the event came in from stripe and not from someplace else
     try {
-      event = stripe.webhooks.constructEvent(payload, sig, endpointSecurit);
-    } catch (e) {
-      console.log("ERROR", e.message);
-      return res.status(400).send({ message: "Webhook error:!!!" + e.message });
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+      console.log("ERROR: ", err.message);
+      return res.status(400).send(`Webhook error: ${err.message}`);
     }
+
+    //Handle the checkout.session.completed event i.e after success on stripe payment gateway
     if (event.type === "checkout.session.completed") {
       const session = event.data.object;
 
-      // Fulfill the order
+      //Fulfill the order -> push the data into firestore and show in orders
       return fulfillOrder(session)
-        .then(() => res.status(200).json({ received: true }))
-        .catch((e) =>
-          res.status(400).send({ message: "WEBHOOK_ERROR:!!! " + e.message })
-        );
+        .then(() => res.status(200))
+        .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
     }
   }
 };
 
+//bodyParser: false bcoz
+//we want the entire request as a stream and no a parsed object
+
+//externalResolver: true bcoz
+//the request is being resolved by stripe and not by us
 export const config = {
   api: {
     bodyParser: false,
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,38 +1,53 @@
-import Image from "next/image";
-import Header from "../components/Header";
-import { useSelector } from "react-redux";
-import { selectItems, selectTotal } from "../slices/basketSlice";
-import CheckoutProducts from "../components/CheckoutProducts";
-import { useSession } from "next-auth/client";
-import Currency from "react-currency-formatter";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
-import Footer from "../components/Footer";
+import { getSession, useSession } from "next-auth/client";
+import Head from "next/head";
+import Image from "next/image";
+import Currency from "react-currency-formatter";
+import { useSelector } from "react-redux";
+import CheckoutProduct from "../components/CheckoutProducts";
+import Header from "../components/Header";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 
+//public key comes from next.config file which calls the env.local
+//always have this line of code outside the function othwise it will load tons and tons of stripe data which is not needed
 const stripePromise = loadStripe(process.env.stripe_public_key);
 
-function checkout() {
-  const items = useSelector(selectItems);
-  const total = useSelector(selectTotal);
+function Checkout() {
   const [session] = useSession();
 
-  const clickHandler = async () => {
+  //items in the basket
+  const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+
+  const createCheckoutSession = async () => {
     const stripe = await stripePromise;
 
+    //call the backend to create a checkout session
     const checkoutSession = await axios.post("/api/create-checkout-session", {
       items: items,
       email: session.user.email,
     });
 
+    //redirect user to stripe Checkout page
     const result = await stripe.redirectToCheckout({
       sessionId: checkoutSession.data.id,
     });
-    if (result.error) alert(result.error.message);
+
+    if (result.error) {
+      alert(result.error.message);
+    }
   };
+
   return (
     <div className="bg-gray-100">
+      <Head>
+        <title>Checkout | Amazon 2.0</title>
+      </Head>
       <Header />
+
       <main className="lg:flex max-w-screen-2xl mx-auto">
+        {/* Left*/}
         <div className="flex-grow m-5 shadow-sm">
           <Image
             src="https://links.papareact.com/ikj"
@@ -40,43 +55,35 @@ function checkout() {
             height={250}
             objectFit="contain"
           />
-          <div className="flex flex-col p-5 bg-white space-y-10">
+
+          <div className="flex flex-col p-5 space-y-10 bg-white">
             <h1 className="text-3xl border-b pb-4">
-              {items.length === 0
-                ? "You Shopping Cart is empty"
-                : "Shopping Basket"}
+              {items.length === 0 ? "Your cart is Empty" : "Shopping Cart"}
             </h1>
-            {items.map(
-              ({
-                id,
-                title,
-                category,
-                image,
-                price,
-                description,
-                hasPrime,
-                rating,
-              }) => (
-                <CheckoutProducts
-                  key={id}
-                  id={id}
-                  title={title}
-                  category={category}
-                  description={description}
-                  price={price}
-                  image={image}
-                  hasPrime={hasPrime}
-                  rating={rating}
-                />
-              )
-            )}
+
+            {/*Always with map have a unique key here we re using i i.e index of the array*/}
+            {items.map((item, i) => (
+              <CheckoutProduct
+                key={i}
+                id={item.id}
+                title={item.title}
+                rating={item.rating}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                hasPrime={item.hasPrime}
+              />
+            ))}
           </div>
         </div>
+
+        {/* Right*/}
         {items.length > 0 && (
-          <div className="flex flex-col bg-white p-14 shadow-md m-5">
+          <div className="flex flex-col bg-white p-10 shadow-md">
             <>
               <h2 className="whitespace-nowrap">
-                Subtotal ({items.length} items):{" "}
+                Subtotal ({items.length} items) :{" "}
                 <span className="font-bold">
                   <Currency quantity={total} currency="INR" />
                 </span>
@@ -84,11 +91,11 @@ function checkout() {
 
               <button
                 role="link"
-                onClick={clickHandler}
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
-                  "from-gray-300 to-gray-500 border-gray-200 text-gray-200 cursor-not-allowed"
+                  "from-gray-300 to-gray-500 border-gray-200 cursor-not-allowed"
                 }`}
               >
                 {!session ? "Sign in to checkout" : "Proceed to checkout"}
@@ -97,9 +104,18 @@ function checkout() {
           </div>
         )}
       </main>
-      <Footer />
     </div>
   );
 }
 
-export default checkout;
+export default Checkout;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
